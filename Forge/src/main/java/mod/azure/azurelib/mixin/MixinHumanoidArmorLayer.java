e57e6d06682d7e28ac9d87359830c94b92a62b81
AzureDoom <jerry99666@gmail.com>
2023-10-06 02:15:49 -0400

    Update MixinHumanoidArmorLayer.java

--- Forge/src/main/java/mod/azure/azurelib/mixin/MixinHumanoidArmorLayer.java
+++ Forge/src/main/java/mod/azure/azurelib/mixin/MixinHumanoidArmorLayer.java
@@ -22,23 +22,23 @@ import net.minecraft.world.item.ItemStack;
  */
 @Mixin(value = HumanoidArmorLayer.class, priority = 700)
 public abstract class MixinHumanoidArmorLayer {
-    @Unique
-    private LivingEntity gl_storedEntity;
-    @Unique
-    private EquipmentSlot gl_storedSlot;
-    @Unique
-    private ItemStack gl_storedItemStack;
+	@Unique
+	private LivingEntity gl_storedEntity;
+	@Unique
+	private EquipmentSlot gl_storedSlot;
+	@Unique
+	private ItemStack gl_storedItemStack;
 
-    @Inject(method = "renderArmorPiece", at = @At(value = "HEAD"))
-    public void armorModelHook(PoseStack poseStack, MultiBufferSource multiBufferSource, LivingEntity livingEntity, EquipmentSlot equipmentSlot, int i, HumanoidModel<LivingEntity> humanoidModel, CallbackInfo ci){
-        this.gl_storedEntity = livingEntity;
-        this.gl_storedSlot = equipmentSlot;
-        this.gl_storedItemStack = livingEntity.getItemBySlot(equipmentSlot);
-    }
+	@Inject(method = "renderArmorPiece", at = @At(value = "HEAD"))
+	public void armorModelHook(PoseStack poseStack, MultiBufferSource multiBufferSource, LivingEntity livingEntity, EquipmentSlot equipmentSlot, int i, HumanoidModel<LivingEntity> humanoidModel, CallbackInfo ci) {
+		this.gl_storedEntity = livingEntity;
+		this.gl_storedSlot = equipmentSlot;
+		this.gl_storedItemStack = livingEntity.getItemBySlot(equipmentSlot);
+	}
 
-    @ModifyArg(method = "renderArmorPiece", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/HumanoidArmorLayer;renderModel(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/item/ArmorItem;Lnet/minecraft/client/model/Model;Lnet/minecraft/resources/ResourceLocation;)V"), 
-    		index = 4)
-    public HumanoidModel<LivingEntity> injectArmor(HumanoidModel<LivingEntity> humanoidModel){
-        return (HumanoidModel<LivingEntity>)RenderProvider.of(this.gl_storedItemStack).getGenericArmorModel(this.gl_storedEntity, this.gl_storedItemStack, this.gl_storedSlot, (HumanoidModel<LivingEntity>) humanoidModel);
-    }
+	@ModifyArg(method = "renderArmorPiece", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/HumanoidArmorLayer;renderModel(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/item/ArmorItem;Lnet/minecraft/client/model/HumanoidModel;ZFFFLjava/lang/String;)V"), 
+			index = 4)
+	public HumanoidModel<LivingEntity> injectArmor(HumanoidModel<LivingEntity> humanoidModel) {
+		return (HumanoidModel<LivingEntity>) RenderProvider.of(this.gl_storedItemStack).getGenericArmorModel(this.gl_storedEntity, this.gl_storedItemStack, this.gl_storedSlot, (HumanoidModel<LivingEntity>) humanoidModel);
+	}
 }
